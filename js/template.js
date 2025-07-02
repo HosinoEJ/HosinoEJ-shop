b464c00236f8db5525ec748a2f768246c2318d3c
星野栄治 <hosinoeiji@gmail.com>
2025-07-02 22:30:07 +0800

    整备更新apera0.1

--- js/template.js
+++ js/template.js
@@ -39,15 +39,7 @@ fetch('../template.html')
   (function () {
     const originalLog = console.log;
     
-    window.hsnej = function () {
-      // 存储元数据到 sessionStorage
-      sessionStorage.setItem('redirectMetadata', JSON.stringify({
-          source: 'custom_page',
-          timestamp: Date.now(),
-      }));
-      // 跳转到 hsnej.html
-      window.location.href = 'hsnej.html';
-  };
+
   
 
     window.info = function () {alert("星野栄治 © 2025｜GNU LICENSEによって転載してください");
